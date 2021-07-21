2d62ba6c82368ccc13af084668d78a6d0752f1fc
McKenna Larson <mklarson14@gmail.com>
2021-07-21 08:28:16 -0700

    set up base server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // set up server
 const http = require('http');
 const express = require('express');
-//const db = require()
+const db = require('./model/db');
 
 const port = process.env.PORT || 3000
 const host = process.env.HOST || '127.0.0.1'
